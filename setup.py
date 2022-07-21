58524890f22043f6f9fe1108213163d42be136e1
Jesse Holzer <jesse.holzer@pnnl.gov>
2022-07-21 08:34:56 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="C3DataUtilities",
+    version="0.1.0",
+    description="GO Competition Challenge 3 utilities for checking problem data and evaluating solutions",
+    author="Jesse Holzer",
+    author_email="jesse.holzer@pnnl.gov",
+    packages=[
+        "data_utilities"
+    ],
+    install_requires=[
+        "pydantic",
+        "datamodel"
+    ]
+)
