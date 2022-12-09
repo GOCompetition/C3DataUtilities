edcc2464bb0a175ab8bfbfb2e40affd9d0191475
Jesse T Holzer <jesse.holzer@pnnl.gov>
2022-12-08 17:44:08 -0800

    update help to show what to do about gurobi

--- check_data.py
+++ check_data.py
@@ -41,21 +41,21 @@ if __name__ == '__main__':
     problem_group = parser.add_mutually_exclusive_group()
     problem_group.add_argument(
         "problem_opt", nargs="?",
-        help="The problem file that we are checking - optional positional argument for backward compatibility, do not use with -p")
-    problem_group.add_argument("-p", "--problem", help="The problem file that we are checking", default=None)
-    parser.add_argument("-s", "--solution", help="The solution file that we are checking", default=None)
-    parser.add_argument("-r", "--scrubbed_problem", default=None, help="File path name to write scrubbed problem file")
+        help="The problem file that we are checking. This is an optional positional argument for backward compatibility and should not be used in combination with the keyword argument \"--problem\".")
+    problem_group.add_argument("-p", "--problem", help="The problem file that we are checking.", default=None)
+    parser.add_argument("-s", "--solution", help="The solution file that we are checking.", default=None)
+    parser.add_argument("-r", "--scrubbed_problem", default=None, help="File path name to write scrubbed problem file.")
 
     # long arguments - these all have defaults and will not be needed as often as the short parameters
     # and therefore do not have short names
     config_group = parser.add_mutually_exclusive_group()
-    config_group.add_argument("--configuration", default=None, help="Configuration file to override default parameter values. Do not use with --parameters")
-    config_group.add_argument("--parameters", default="{}", help="JSON string containing configuration parameter values to override the defaults. Do not use with --configuration")
-    parser.add_argument("--summary_csv", default=summary_csv_file, help="Summary output file - CSV format", )
-    parser.add_argument("--summary_json", default=summary_json_file, help="Summary output file - JSON format", )
-    parser.add_argument("--data_errors", default=data_errors_file, help="Data errors output file")
-    parser.add_argument("--ignored_errors", default=ignored_errors_file, help="Ignored errors output file")
-    parser.add_argument("--solution_errors", default=solution_errors_file, help="Solution errors output file")
+    config_group.add_argument("--configuration", default=None, help="Configuration file to override default parameter values. Do not use this argument in combination with \"--parameters\".")
+    config_group.add_argument("--parameters", default="{}", help="JSON string containing configuration parameter values to override the defaults. E.g. adding \"--parameters \'{\"do_opt_solves\": false}\'\" to the Python command disables certain data checks that rely on Gurobi to solve an optimization problem. Do not use this argument in combination with \"--configuration\".")
+    parser.add_argument("--summary_csv", default=summary_csv_file, help="Summary output file - CSV format.", )
+    parser.add_argument("--summary_json", default=summary_json_file, help="Summary output file - JSON format.", )
+    parser.add_argument("--data_errors", default=data_errors_file, help="Data errors output file.")
+    parser.add_argument("--ignored_errors", default=ignored_errors_file, help="Ignored errors output file.")
+    parser.add_argument("--solution_errors", default=solution_errors_file, help="Solution errors output file.")
                         # metavar='KEY=VALUE',
                         # nargs='+',
                         # help=(
